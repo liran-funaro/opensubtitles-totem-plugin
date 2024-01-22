d414328937ab23e8bfaa0e2c08c22dd6e1deb6bf
Liran Funaro <liran.funaro@gmail.com>
2024-01-23 00:24:29 +0200

    Add space chars

--- opensubtitles/api/filenameparser.py
+++ opensubtitles/api/filenameparser.py
@@ -219,8 +219,7 @@ AUDIO_LABELS = [
     "DDP", "DDP-A",
     "MultiAudio",
 ]
-
-SINGLE_SEP = re.compile(r"[.\-_ ·*:\t\n[\]]", re.IGNORECASE)
+SINGLE_SEP = re.compile(r"[.\-_ ·*:+\t\n[\]]", re.IGNORECASE)
 SEP = re.compile(rf"{SINGLE_SEP.pattern}+", re.IGNORECASE)
 OPT_SEP = re.compile(rf"{SINGLE_SEP.pattern}*", re.IGNORECASE)
 BA = f"(?:$|(?={SINGLE_SEP.pattern}))"
